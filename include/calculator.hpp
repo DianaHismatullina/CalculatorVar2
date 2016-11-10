876e1deac4fec32c6aeb53dae5aae8de74e041f1
DianaHismatullina <dianarh@rambler.ru>
2016-11-10 17:17:05 +0300

    Create calculator.hpp

---
+++ include/calculator.hpp
@@ -0,0 +1,6 @@
+    double summa(double a, double b)
+    double sub(double a, double b)
+    double mult(double a, double b)
+    double dev(double a, double b)
+    double pow(double a, double b) 
+    double sqrt(double a)
